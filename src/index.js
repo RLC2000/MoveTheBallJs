07a9060102ec23bdc94bf2fec3190844ee692193
Rasham Lal Chhetri <56154987+RLC2000@users.noreply.github.com>
2022-07-07 16:16:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+/*
+
+ NOTE FOR FUTURE ME:
+transform: translate() does not alter the document flow. The element's original space is 
+unaffected and still visible in it's original position.
+
+
+Normally in css when we do a translate of xad y axis we move form old position to new position just 1 transform 
+but in javascript here we want to attempt multiple transform for translateX and translateY so if we
+do a translateX of constant data then in that case motion will be consatant only once as the orifinal position remains unaffected
+so for the logic to work we perform :
+  original position+current postion+expected motion.
+*/
+
 import "./styles.css";
 var ele = document.querySelector("div");
 window.addEventListener("keypress", hlpr);
